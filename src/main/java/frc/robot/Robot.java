f95ae083c4bbc4a3adfbaf2afd92b28893fbd47b
Gulksie <shaker02@live.ca>
2020-03-05 11:29:18 -0500

    Code that probably doesn't work

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,13 +12,18 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.Servo;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.ColorSensorV3;
 
@@ -37,9 +42,6 @@ public class Robot extends TimedRobot {
   // shooter
   private Shooter shooter;
   private double shooterSpeed;
-
-  // servo
-  private Servo twoWire;
 
   // disk spinner
   final Color colourBlue = new Color(0, 1, 1);
@@ -60,9 +62,14 @@ public class Robot extends TimedRobot {
   // intake
   private WPI_VictorSPX[] intakeMotors = { new WPI_VictorSPX(41), new WPI_VictorSPX(42) };
   private WPI_VictorSPX intakeWrist = new WPI_VictorSPX(40);
-  private AnalogInput wristPotentiometer = new AnalogInput(0);
-  private int intakeState = 0; // 0=up, 1 = falling, 2 = down, 3 = rising, starts up
-  private long intakeTime;
+  // private AnalogInput wristPotentiometer = new AnalogInput(0);
+  private boolean runBallTrack = false;
+  private boolean intaking = false;
+  private boolean lastDIO0 = true;
+  private boolean lastDIO1 = true;
+  private long ballTrackTimer = -1;
+  private DigitalInput dio0 = new DigitalInput(0);
+  private DigitalInput dio1 = new DigitalInput(1);
 
   /**
    * This function is run when the robot is first started up and should be used
@@ -88,17 +95,45 @@ public class Robot extends TimedRobot {
    * This runs after the mode specific periodic functions, but before LiveWindow
    * and SmartDashboard integrated updating.
    */
+
+  DigitalInput[] dis = { new DigitalInput(2), new DigitalInput(3) };
+  AnalogInput[] ans = { new AnalogInput(0), new AnalogInput(1) };
+
   @Override
   public void robotPeriodic() {
-    controller.setRumble(RumbleType.kLeftRumble, 0.2d);
-    controller.setRumble(RumbleType.kRightRumble, 0.2d);
+
+    SmartDashboard.putBoolean("DIO 0", dio0.get());
+    SmartDashboard.putBoolean("DIO 1", dio1.get());
+
+    for (int i = 0; i < 2; i++) {
+      SmartDashboard.putBoolean("DIO " + (i + 2), dis[i].get());
+    }
+
+    for (int i = 0; i <= 1; i++) {
+      SmartDashboard.putNumber("Analog " + i, ans[i].getVoltage());
+    }
+
+    SmartDashboard.putNumber("Shooter Top", shooter.top.getSensorCollection().getQuadratureVelocity() * 10f / 4096f);
+    SmartDashboard.putNumber("Shooter Bottom",
+        shooter.bottom.getSensorCollection().getQuadratureVelocity() * 10f / 4096f);
+    SmartDashboard.putNumber("Drive Left", drive.leftTalon.getSensorCollection().getQuadraturePosition() / 4096f);
+    SmartDashboard.putNumber("Drive Right", drive.rightTalon.getSensorCollection().getQuadraturePosition() / 4096f);
+
+    SmartDashboard.putNumber("Shooter Top Setpoint", shooter.top.getClosedLoopTarget() * 10f / 4096f);
+    SmartDashboard.putNumber("Shooter Bottom Setpoint", shooter.bottom.getClosedLoopTarget() * 10f / 4096f);
+
+    SmartDashboard.putNumber("Shooter controller speed", shooterSpeed);
+
   }
 
   @Override
   public void autonomousInit() {
-    intakeState = 0;
     spinning = false;
     colourCount = 0;
+    shooterSpeed = 0;
+    runBallTrack = false;
+    lastDIO0 = true;
+    lastDIO1 = true;
   }
 
   /**
@@ -114,9 +149,9 @@ public class Robot extends TimedRobot {
 
   // CONTROLS
   // X button: intake
-  // Y button: disk spinner (control pannel)
-  // A button: climb
-  // B button: shooter *eventually*
+  // Y button: nothin
+  // A button: nothin
+  // B button: shooter *eventually*???
   // 2 stick drive - left stick Y, right stick X
   //
   // OVERRIDES
@@ -127,6 +162,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+
     // controller and joystick axises
     double leftXValue = controller.getRawAxis(0);
     double leftYValue = controller.getRawAxis(1);
@@ -134,10 +170,6 @@ public class Robot extends TimedRobot {
     double rightYValue = controller.getRawAxis(5);
     double joystickX = joystick.getRawAxis(0);
     double joystickY = joystick.getRawAxis(1);
-
-    // double toSet = 0.44 * leftYValue + 0.5;
-
-    // twoWire.set(toSet);
 
     // System.out.println("Controller Value: " + leftYValue);
     // System.out.println("Set value: " + toSet);
@@ -155,102 +187,97 @@ public class Robot extends TimedRobot {
     if (joystick.getRawButton(1)) {
       shooter.setSpeed(joystickY);
     } else {
-      shooter.setSpeed(shooterSpeed);
+      /*
+       * shooter.top.set(ControlMode.Velocity, (1 * shooterSpeed) * (4096f / 10f));
+       * shooter.bottom.set(ControlMode.Velocity, (1 * -shooterSpeed) * (4096f /
+       * 10f));
+       */
     }
 
     //
     // intake controls
     //
-    if (controller.getXButtonPressed()) {
-      intakeState = intakeState >= 1 ? 0 : 1;
+
+    // intake
+    if (joystick.getRawButton(2)) { // intake override
+      intakeMotors[0].set(joystickY);
+    } else {
+      if (controller.getXButtonPressed()) {
+        intaking = !intaking;
+      }
+
+      intakeMotors[0].set(intaking ? -0.5d : 0);
     }
 
-    // manual overrides
-    if (joystick.getRawButton(2) || joystick.getRawButton(3)) {
-      if (joystick.getRawButton(2)) {
-        intakeMotors[0].set(joystickY);
-      }
-      if (joystick.getRawButton(3)) {
-        intakeMotors[1].set(joystickX);
-      }
+    // ball track
+    if (joystick.getRawButton(3)) { // override
+      intakeMotors[1].set(joystickX);
     } else {
-      if (intakeState == 1) {
-        // intake for me
-        intakeMotors[0].set(-0.75d);
-        intakeMotors[1].set(0.5d);
-      } else if (intakeState == 0) {
-        intakeMotors[0].set(0d);
-        intakeMotors[1].set(0d);
+
+      boolean currentDIO0 = dio0.get();
+      if (lastDIO0 != currentDIO0) {// its changed
+        if (!currentDIO0) { // ball just entered, run the ball track if not already
+          runBallTrack = true;
+        }
       }
+
+      boolean currentDIO1 = dio1.get();
+      if (lastDIO1 != currentDIO1) {
+        if (!currentDIO1) { // we just activated, start the timer
+          ballTrackTimer = System.currentTimeMillis();
+        }
+      }
+
+      double volts = RobotController.getBatteryVoltage();
+      double wait = 12.6d * (270d / volts); // wait for 270 milis at 12.6V (what was tested at), otherwise wait more
+
+      if (ballTrackTimer > -1 && System.currentTimeMillis() >= (ballTrackTimer + Math.round(wait))) {
+        runBallTrack = false;
+        ballTrackTimer = -1;
+      }
+
+      intakeMotors[1].set(runBallTrack ? 0.5d : 0);
+      lastDIO0 = currentDIO0;
+      lastDIO1 = currentDIO1;
     }
 
     //
     // colour picker thingy controls
     //
 
-    String gameColour = DriverStation.getInstance().getGameSpecificMessage();
-    Color readColour = colourSensor.getColor();
-
-    if (gameColour.length() > 0) {
-      System.out.println(gameColour);
-    }
-
-    if (joystick.getRawButton(4)) { // manual override
-      diskSpinner.set(joystickY);
-    } else {
-
-      if (controller.getYButtonPressed()) { // start the spinning process
-        // in if statement so spinning doesn't accidentally get set to false
-        spinning = true;
-
-        if (gameColour.length() > 0) {
-          switch (gameColour.charAt(0)) {
-            case 'B':
-              colourToFind = colourBlue;
-              break;
-            case 'G':
-              colourToFind = colourGreen;
-              break;
-            case 'R':
-              colourToFind = colourRed;
-              break;
-            case 'Y':
-              colourToFind = colourYellow;
-              break;
-            default:
-              // well fuck me i guess
-              colourToFind = null;
-              break;
-          }
-        }
-      }
-
-      if (spinning) {
-        if (gameColour.length() <= 0) { // phase 1, we just want to spin
-          // basically spin it until the colour changes 24 times
-
-          // check if colour has changed since last time
-          if (readColour == lastColour) {
-            colourCount++;
-          }
-
-          if (colourCount <= 24) { // we've done 3 rotations, stop
-            diskSpinner.set(0d);
-            spinning = false;
-            colourCount = 0;
-          }
-
-        } else if (colourToFind != null) { // phase 2, we need to match colour
-          if (readColour == colourToFind) {
-            // we've found the colour, stop spinning
-            diskSpinner.set(0d);
-            spinning = false;
-          }
-        }
-      }
-
-      diskSpinner.set(spinning ? 0.5d : 0d);
-    }
+    /*
+     * String gameColour = DriverStation.getInstance().getGameSpecificMessage();
+     * Color readColour = colourSensor.getColor();
+     * 
+     * if (gameColour.length() > 0) { System.out.println(gameColour); }
+     * 
+     * if (joystick.getRawButton(4)) { // manual override
+     * diskSpinner.set(joystickY); } else {
+     * 
+     * if (controller.getYButtonPressed()) { // start the spinning process // in if
+     * statement so spinning doesn't accidentally get set to false spinning = true;
+     * 
+     * if (gameColour.length() > 0) { switch (gameColour.charAt(0)) { case 'B':
+     * colourToFind = colourBlue; break; case 'G': colourToFind = colourGreen;
+     * break; case 'R': colourToFind = colourRed; break; case 'Y': colourToFind =
+     * colourYellow; break; default: // well fuck me i guess colourToFind = null;
+     * break; } } }
+     * 
+     * if (spinning) { if (gameColour.length() <= 0) { // phase 1, we just want to
+     * spin // basically spin it until the colour changes 24 times
+     * 
+     * // check if colour has changed since last time if (readColour == lastColour)
+     * { colourCount++; }
+     * 
+     * if (colourCount <= 24) { // we've done 3 rotations, stop diskSpinner.set(0d);
+     * spinning = false; colourCount = 0; }
+     * 
+     * } else if (colourToFind != null) { // phase 2, we need to match colour if
+     * (readColour == colourToFind) { // we've found the colour, stop spinning
+     * diskSpinner.set(0d); spinning = false; } } }
+     * 
+     * diskSpinner.set(spinning ? 0.5d : 0d); }
+     */
 
     if (!controller.getBumper(Hand.kLeft)) {
       robotDrive.arcadeDrive(-rightXValue, -leftYValue); // make robot move
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,20 +1,27 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 
 public class Shooter {
-    private WPI_TalonSRX top;
-    private WPI_TalonSRX bottom;
-    private WPI_TalonSRX pivot;
-    private AnalogInput potentiometer;
+    public WPI_TalonSRX top;
+    public WPI_TalonSRX bottom;
+    public WPI_TalonSRX pivot;
 
     public Shooter() {
         top = new WPI_TalonSRX(10);
+        top.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+        top.setSensorPhase(true);
+
         bottom = new WPI_TalonSRX(11);
-        pivot = new WPI_TalonSRX(12);
-        potentiometer = new AnalogInput(1);
+        bottom.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+        bottom.setSensorPhase(false);
+        // bottom.setInverted(false);
+        // pivot = new WPI_TalonSRX(12);
+        // potentiometer = new AnalogInput(1);
     }
 
     public void shoot() {
